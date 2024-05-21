5e15622ed9e697457145110d3b1c808424b97259
Mauro <114999241+mauro-vb@users.noreply.github.com>
2024-05-21 09:56:58 +0200

    latest stage

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 500.0
-const ACCELERATION = 50
+const SPEED = 800.0
+const ACCELERATION = 35
 const DECELERATION_THRESHOLD = 50
 
 
@@ -26,29 +26,5 @@ func _physics_process(_delta):
 
 	else:
 		decelerate.call()
-
-	move_and_slide()
-
-func _physics_process_(delta):
-	var decelerate = func(): velocity.y = lerp(velocity.y, 0.0, .05) # Lambda function to decelerate
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var up = Input.is_action_pressed("up")
-	var down = Input.is_action_pressed("down")
-	
-	if down and up:
-		decelerate.call()
-	elif down:
-		if velocity.y >= -DECELERATION_THRESHOLD:
-			velocity.y =  max(ACCELERATION*delta, SPEED)
-		else:
-			decelerate.call()
-	elif up:
-		if velocity.y <= DECELERATION_THRESHOLD:
-			velocity.y =  min(-ACCELERATION*delta, -SPEED)
-		else:
-			decelerate.call()
-	else:
-		decelerate.call()
-
+		
 	move_and_slide()
