0a771c9ab74b597042fa1544645fd66201a20a8f
Mauro <114999241+mauro-vb@users.noreply.github.com>
2024-05-21 17:36:15 +0200

    added base obstacle

--- scripts/components/hitbox_component.gd
+++ scripts/components/hitbox_component.gd
@@ -3,6 +3,8 @@ class_name HitBoxComponent
 
 @export var health_component : HealthComponent
 
+var invulnerable := false
+
 func damage(damage_amount:int):
 	if health_component:
 		health_component.damage(damage_amount)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 800.0
-const ACCELERATION = 35
-const DECELERATION_THRESHOLD = 50
+const SPEED = 900.0
+const ACCELERATION = 55
 
 
+func wants_move_up():
+	return Input.is_action_pressed("up")
+	
+func wants_move_down():
+	return Input.is_action_pressed("down")
 
 func _physics_process(_delta):
 	var decelerate = func(): velocity.y = lerp(velocity.y, 0.0, .05) # Lambda function to decelerate
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var up = Input.is_action_pressed("up")
-	var down = Input.is_action_pressed("down")
-	print(velocity.y)
+	var up = wants_move_up()
+	var down = wants_move_down()
+
 	if down and up:
 		decelerate.call()
 	elif down:
@@ -28,3 +32,7 @@ func _physics_process(_delta):
 		decelerate.call()
 		
 	move_and_slide()
+
+
+func _on_hit_box_area_entered(area):
+	print(area.name)
